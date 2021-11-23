3fabe1bd9b793ee9f5c3212fd269ca4845180efa
ihor <igchup1985@gmail.com>
2021-11-23 23:47:21 +0200

    add additional5

---
+++ lesson5/additional5/script_ad5.js
@@ -0,0 +1,34 @@
+// - Дано натуральное число n. Выведите все числа от 1 до n.
+const showNumber = (numb = 1, n) => {
+    console.log(numb);
+    numb++;
+    if (numb > n) {
+        return;
+    }
+    showNumber(numb, n);
+}
+showNumber(1, 15)
+
+// - Даны два целых числа A и В . Выведите все числа от A до B включительно,
+// в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+const showNumberFromAtoB = (numA, numB) => {
+   if (numA <= numB) {
+       console.log(numA);
+       numA++;
+       if (numA > numB) {
+           return;
+       }
+   }
+   else if (numA >= numB) {
+       console.log(numA);
+       numA--;
+       if (numA < numB) {
+           return;
+       }
+   }
+showNumberFromAtoB(numA, numB);
+}
+showNumberFromAtoB(1, 12);
+
+
+
